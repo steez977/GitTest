a25136a1e1707851754c33b271f22f3b47f6b8f5
Stijn Vervaet <stijnvervaet@gmail.com>
2020-03-07 18:01:49 +0000

    Material cleanup , rocket inpit

---
+++ Assets/Rocket.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // print("Update");
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            print("space pressed!");
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("Turn left!");
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            print("Turn right!");
+        }
+
+    }
+}
